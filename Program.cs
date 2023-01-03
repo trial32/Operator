ee37d73cfef19ca2bf64e82db582ce5cd6200787
icmosoftware <ozendeniz@gmail.com>
2023-01-03 23:02:09 +0300

    Modified

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int x=20;
+int y=30;
+
+bool checkSame= x==y;
+
+Console.WriteLine(checkSame);
+
+x*=2;
+Console.WriteLine(x);
+
+bool isSuccess=false;
+
+if (checkSame && isSuccess)
+    Console.WriteLine("GOOD");
+    else
+    Console.WriteLine("BAD");
